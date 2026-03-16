9bb7422816527ed1cf9e7cebbc7f86ce022518a0
Alex <35335834+Evlampyev@users.noreply.github.com>
2026-03-16 22:55:41 +0500

    some bags delete-3

--- users_app/templatetags/custom_filters.py
+++ users_app/templatetags/custom_filters.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def multiply(value, arg):
     """Умножает значение на аргумент"""
@@ -9,3 +10,31 @@ def multiply(value, arg):
         return int(value) * int(arg)
     except (ValueError, TypeError):
         return 0
+
+
+@register.filter
+def star_rating(value):
+    """Возвращает HTML со звездами на основе количества задач"""
+    try:
+        stars_count = value // 10
+        if stars_count == 0:
+            return ''
+
+        stars_html = ''
+        for i in range(stars_count):
+            stars_html += '<i class="fas fa-star text-warning" title="10+ задач"></i>'
+
+        # Добавляем полузвезду за остаток
+        remainder = value % 10
+        if remainder >= 5:
+            stars_html += '<i class="fas fa-star-half-alt text-warning" title="5+ задач"></i>'
+
+        return stars_html
+    except (TypeError, ValueError):
+        return ''
+
+
+@register.filter
+def get_range(value):
+    """Возвращает диапазон для цикла"""
+    return range(value)
