98824c173edb96f2559d3697eeb9ffe8149d89e8
Alex <35335834+Evlampyev@users.noreply.github.com>
2026-03-22 22:59:03 +0500

    collection is create and modify

--- TempTestsProgram/settings.py
+++ TempTestsProgram/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
             "class": "logging.FileHandler",
             "filename": "debug.log",
             "encoding": "utf-8",
-            "mode": "w",  # Добавьте эту строку - 'w' для перезаписи при каждом запуске
+            "mode": "w",
         },
     },
     "root": {
